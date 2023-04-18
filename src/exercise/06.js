257667182b2bff99243218a46dfad7900088e8eb
Abhilash Nair <abhilashnair@MacBook-Pro.domain>
2023-04-18 15:40:39 -0600

    complete initial exercise of using useEffect for http request

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,15 +1,16 @@
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import React, {useState, useEffect} from 'react'
 // 🐨 you'll want the following additional things from '../pokemon':
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {PokemonDataView, PokemonForm, PokemonInfoFallback, fetchPokemon} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = useState(null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
@@ -20,17 +21,33 @@ function PokemonInfo({pokemonName}) {
   //   fetchPokemon('Pikachu').then(
   //     pokemonData => {/* update all the state here */},
   //   )
+  useEffect(() => {
+    // if pokemon name not given, do nothing
+    if (!pokemonName) {
+      return
+    }
+    // reset to loading state before fetching
+    setPokemon(null)
+    fetchPokemon(pokemonName).then(pokemonData => {
+      setPokemon(pokemonData)
+    })
+  }, [pokemonName])
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  if (!pokemonName) {
+    return 'Submit a pokemon'
+  }
+  if (!pokemon) {
+    return <PokemonInfoFallback name={pokemonName}/>
+  }
 
-  // 💣 remove this
-  return 'TODO'
+  return <PokemonDataView pokemon={pokemon} />
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
+  const [pokemonName, setPokemonName] = useState('')
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
